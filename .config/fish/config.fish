bc94fc1f2b4b32864f3eff81c3707ed494666aaa
Nicolas Buzeta <nicolas@buzeta.net>
2026-01-05 00:08:58 -0300

    fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set -g fish_key_bindings fish_vi_key_bindings
+end
+
+set -gx PATH /home/nicobuzeta/.local/bin $PATH
